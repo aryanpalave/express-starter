d4a5d0c76fc47f3b1484f82890ef450b39b01fa6
aryannn108 <75058883+aryannn108@users.noreply.github.com>
2025-01-20 17:03:43 -0800

    commit3

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET users music. */
+router.get('/music', function(req, res, next) {
+  res.send('Drake, Travis, Weeknd');
+});
+
+/* GET users fav color. */
+router.get('/color', function(req, res, next) {
+  res.send('respond with blue, green');
+});
+
 export default router;
